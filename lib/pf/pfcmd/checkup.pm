b1375f43b2439713838185ce33798246ab8dcc5f
Durand Fabrice <fdurand@inverse.ca>
2015-04-27 13:57:23 -0400

    Switch Range

--- lib/pf/pfcmd/checkup.pm
+++ lib/pf/pfcmd/checkup.pm
@@ -847,7 +847,7 @@ sub switches {
             }
         }
         # check for valid switch IP
-        unless ( valid_mac_or_ip($section) ) {
+        unless ( valid_mac_or_ip($section) || valid_ip_range($section) ) {
             add_problem( $WARN, "switches.conf | Switch IP is invalid for switch $section" );
         }
 
