b083db9aa78137e23be7dada3a3083fdecd9630f
Durand Fabrice <fdurand@inverse.ca>
2015-05-04 10:10:06 -0400

    Added switch range to dynamic-clients in freeradius

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -8,3 +8,7 @@
 
 ALTER TABLE class
     ADD `external_command` varchar(255) DEFAULT NULL;
+-- Add a column to radius_nas to order the nas list
+--
+
+ALTER TABLE radius_nas ADD `position` INT FIRST;
